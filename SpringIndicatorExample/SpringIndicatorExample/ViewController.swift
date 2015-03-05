e4fc0f108da6ed551bbe25e0552f765164e9b09b
ito_kyohei <ito_kyohei@cyberagent.co.jp>
2015-03-06 03:08:50 +0900

    add example

---
+++ SpringIndicatorExample/SpringIndicatorExample/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  SpringIndicatorExample
+//
+//  Created by Kyohei Ito on 2015/03/06.
+//  Copyright (c) 2015年 kyohei_ito. All rights reserved.
+//
+
+import UIKit
+import SpringIndicator
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaultIndicator = SpringIndicator(frame: CGRect(x: 100, y: 100, width: 60, height: 60))
+        view.addSubview(defaultIndicator)
+        defaultIndicator.startAnimation()
+        
+        let colors = [UIColor.redColor(), UIColor.blueColor(), UIColor.greenColor(), UIColor.yellowColor()]
+        var colorsIndex = 0
+        
+        let colorIndicator = SpringIndicator(frame: CGRect(x: 300, y: 100, width: 20, height: 20))
+        colorIndicator.lineWidth = 2
+        colorIndicator.lotateDuration = 1
+        colorIndicator.strokeDuration = 0.5
+        colorIndicator.intervalAnimationsHandler = { indicator in
+            indicator.lineColor = colors[colorsIndex++]
+            
+            if colorsIndex >= colors.count {
+                colorsIndex = 0
+            }
+        }
+        view.addSubview(colorIndicator)
+        colorIndicator.startAnimation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
